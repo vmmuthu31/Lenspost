9d6f392233bae3b3bdfb4a15b0f8562e3753dfc5
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-04 15:40:12 +0530

    updated the next config with lens

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: [
+      "ipfs.infura.io",
+      "statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com",
+      "lens.infura-ipfs.io",
+      "source.unsplash.com",
+      "",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
