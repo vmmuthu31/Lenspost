5c4f2b369ed9001f365f97d9d07d8b913163c7f4
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-04 15:42:01 +0530

    updated the lenspost with lens api.js

---
+++ api.js
@@ -0,0 +1,57 @@
+import { createClient } from "urql";
+
+const APIURL = "https://api.lens.dev";
+
+export const client = new createClient({
+  url: APIURL,
+});
+
+export const exploreProfiles = `
+  query ExploreProfiles {
+    exploreProfiles(request: { sortCriteria: MOST_FOLLOWERS }) {
+      items {
+        id
+        name
+        bio
+        handle
+        picture {
+          ... on MediaSet {
+            original {
+              url
+            }
+          }
+        }
+        stats {
+          totalFollowers
+        }
+      }
+    }
+  }
+`;
+
+export const getPublications = `
+  query Publications($id: ProfileId!, $limit: LimitScalar) {
+    publications(request: {
+      profileId: $id,
+      publicationTypes: [POST],
+      limit: $limit
+    }) {
+      items {
+        __typename 
+        ... on Post {
+          ...PostFields
+        }
+      }
+    }
+  }
+  fragment PostFields on Post {
+    id
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+  }
+  fragment MetadataOutputFields on MetadataOutput {
+    content
+  }
+`;
